e498a0d0166ba8d0fc0bee81b58c1e4133c0f185
ziad nasr-eldin <ziad11nasr11@gmail.com>
2026-06-27 19:40:30 +0300

    feat: implement navigation between pages

--- src/books/urls.py
+++ src/books/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.book_list, name='book_list'),
+    path('', views.book_list, name='books_list'),
     path('update/<slug:slug>/', views.update_book, name='update_book'),
     path('delete/<slug:slug>/', views.delete_book, name='delete_book'),
     
